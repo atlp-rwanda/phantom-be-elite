3d7b7916ed82c8499aece56c81faec2ab63d4776
Benjamin shyaka <benshyaka99@gmail.com>
2022-06-07 13:22:30 +0200

    ch(setup integration): -installing modules - adding test script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import app from './app';
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => { console.log("Server listening on port " + port) });
