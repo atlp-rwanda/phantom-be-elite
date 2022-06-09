d9f63808d9025d44c6677cc78573f293753b54c2
Hodal Muheto <mhthodol@gmail.com>
2022-06-09 21:05:52 +0200

    chore(support-multiple-language):finished settup language
    now user can be able to select the language
    the selected language is appplied to content of application
    
    this feature is in review atatus

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import swaggerUi from "swagger-ui-express";
 import swaggerDoc from "./api.json";
+import i18n from './configs/i18n.js';
 
 
 const server = express();
+server.use(i18n.init);
 
 // default route
 server.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: "Welcome to Phantom API" })
+    res.status(200).json({ success: true, message: res.__("welcome")})
+});
+
+server.get('/language-test', (req, res) => {
+  res.status(200).json({ success: true, message: res.__("language")})
 });
 
 server.use(express.json());
@@ -16,6 +22,7 @@ server.use(
     swaggerUi.serve,
     swaggerUi.setup(swaggerDoc, { explorer: true })
 );
+
 
 
 export default server;
