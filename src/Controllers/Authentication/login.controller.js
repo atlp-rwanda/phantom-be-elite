f3b58932bd8c6a959c51e1c9c5406fd1da4dd717
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-21 09:10:18 +0200

    ft(Sign in): Sign in to Phantom > > - The driver, administrator, and Operator should be able to sign in to app by providing their email & password. > - The API should return a JW token on a successful login. > - Appropriate error messages are displayed on unsuccessful attempts - The administrator should be able to sign in to the app using pre-configured login credentials.
      [ Finishes #8 ]

--- src/Controllers/Authentication/login.controller.js
+++ src/Controllers/Authentication/login.controller.js
@@ -11,28 +11,43 @@ export const login = async (req, res) => {
 		`SELECT * FROM public."Users" WHERE email LIKE '%${email}%' LIMIT 1`
 	);
 
-	if (!user.rowCount) {
-		return res
-			.status(401)
-			.send({ success: false, message: `Invalid email or password` });
+	if (email == "admin@gmail.com" && password == "password") {
+		const userData = {
+			name: "Admin User",
+			role: "admin",
+			email: email,
+			createdAt: new Date(),
+			id: "1",
+		};
+		const token = signToken(JSON.stringify(userData));
+		return res.status(200).json({
+			success: true,
+			message: "successfully Admin logged in",
+			token,
+		});
+	} else {
+		if (!user.rowCount) {
+			return res
+				.status(401)
+				.send({ success: false, message: `Invalid email or password` });
+		}
+		const isPasswordValid = await verify(password, user.rows[0].password);
+		if (!isPasswordValid)
+			return res
+				.status(401)
+				.json({ success: false, message: "Invalid email or password" });
+		const userData = {
+			name: user.rows[0].name,
+			role: user.rows[0].role,
+			email: user.rows[0].email,
+			createdAt: user.rows[0].createdAt,
+			id: user.rows[0].id,
+		};
+		const token = signToken(JSON.stringify(userData));
+		return res.status(200).json({
+			success: true,
+			message: "successfully logged in",
+			token,
+		});
 	}
-	const isPasswordValid = await verify(password, user.rows[0].password);
-	if (!isPasswordValid)
-		return res
-			.status(401)
-			.json({ success: false, message: "Invalid email or password" });
-
-	const userData = {
-		name: user.rows[0].name,
-		role: user.rows[0].role,
-		email: user.rows[0].email,
-		createdAt: user.rows[0].createdAt,
-		id: user.rows[0].id,
-	};
-	const token = signToken(JSON.stringify(userData));
-	return res.status(200).json({
-		success: true,
-		message: "successfully logged in",
-		token,
-	});
 };
