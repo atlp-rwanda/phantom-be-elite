f458fdf434dd5e00bd9e5ab2cb2699fa93ec009e
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-06-21 12:56:01 +0200

    ft(DOCUMENTATION): document operator & driver endpoints

--- src/Database/migrations/20220614165646-create-user.js
+++ src/Database/migrations/20220614165646-create-user.js
@@ -2,48 +2,48 @@
 
 "use strict";
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable("Users", {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			name: {
-				type: Sequelize.STRING,
-			},
-			email: {
-				type: Sequelize.STRING,
-			},
-			id_number: {
-				type: Sequelize.STRING,
-			},
-			permit_id: {
-				type: Sequelize.STRING,
-			},
-			phone: {
-				type: Sequelize.INTEGER,
-			},
-			password: {
-				type: Sequelize.STRING,
-			},
-			role: {
-				type: Sequelize.ENUM,
-				values: ["admin", "operator", "driver", "client"],
-				defaultValue: "client",
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-			},
-		});
-	},
-	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable("Users");
-	},
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("Users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            email: {
+                type: Sequelize.STRING,
+            },
+            id_number: {
+                type: Sequelize.STRING,
+            },
+            permit_id: {
+                type: Sequelize.STRING,
+            },
+            phone: {
+                type: Sequelize.INTEGER,
+            },
+            password: {
+                type: Sequelize.STRING,
+            },
+            role: {
+                type: Sequelize.ENUM,
+                values: ["admin", "operator", "driver", "client"],
+                defaultValue: "client",
+            },
+            createdAt: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("Users");
+    },
 };
