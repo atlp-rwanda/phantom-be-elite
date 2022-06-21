5dacfc9bf283ac095b42437f6f0d6c88dd871cf5
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-21 09:14:18 +0200

    ft(Sign in): Sign in to Phantom > > - The driver, administrator, and Operator should be able to sign in to app by providing their email & password. > - The API should return a JW token on a successful login. > - Appropriate error messages are displayed on unsuccessful attempts - The administrator should be able to sign in to the app using pre-configured login credentials.
      [ Finishes #8 ]

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -20,11 +20,30 @@ describe("POST API /api/v1/auth/login", () => {
 		email: "test@gmail.com",
 		password: "password",
 	};
+	const user4 = {
+		email: "admin@gmail.com",
+		password: "password",
+	};
 	it("it should successfully login and return 200 and get token", (done) => {
 		chai
 			.request(index)
 			.post("/api/v1/auth/login")
 			.send(user)
+			.end((err, res) => {
+				if (err) return done(err);
+				token = res.body.token;
+				expect(res.status).to.be.equal(200);
+				expect(res.body).to.have.property("success");
+				expect(res.body).to.have.property("message");
+				expect(res.body).to.have.property("token");
+				return done();
+			});
+	});
+	it("Admin should successfully login and return 200 and get token", (done) => {
+		chai
+			.request(index)
+			.post("/api/v1/auth/login")
+			.send(user4)
 			.end((err, res) => {
 				if (err) return done(err);
 				token = res.body.token;
