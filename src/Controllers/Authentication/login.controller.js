edb998f5cde1f763e198a98e50300a6b94bb4b08
Hodal Muheto <mhthodol@gmail.com>
2022-06-22 15:10:43 +0200

    feature(logout):added authentication
    the user who want to logout are authenticated
    Now user can access the prptected routes
    
    the feature is in WIP#21

--- src/Controllers/Authentication/login.controller.js
+++ src/Controllers/Authentication/login.controller.js
@@ -3,51 +3,82 @@
 import pool from "../../Database/database";
 import { verify } from "../../middleware/hash-password";
 import { signToken } from "../../middleware/jwt";
+import jwt from "jsonwebtoken";
+import handleResponse from "../handleResponse";
+import Token from "../../models/token";
 
 export const login = async (req, res) => {
-	const { password, email } = req.body;
+  const { password, email } = req.body;
 
-	let user = await pool.query(
-		`SELECT * FROM public."Users" WHERE email LIKE '%${email}%' LIMIT 1`
-	);
+  let user = await pool.query(
+    `SELECT * FROM public."Users" WHERE email LIKE '%${email}%' LIMIT 1`
+  );
 
-	if (email == "admin@gmail.com" && password == "password") {
-		const userData = {
-			name: "Admin User",
-			role: "admin",
-			email: email,
-			createdAt: new Date(),
-			id: "1",
-		};
-		const token = signToken(JSON.stringify(userData));
-		return res.status(200).json({
-			success: true,
-			message: "successfully Admin logged in",
-			token,
-		});
-	} else {
-		if (!user.rowCount) {
-			return res
-				.status(401)
-				.send({ success: false, message: `Invalid email or password` });
-		}
-		const isPasswordValid = await verify(password, user.rows[0].password);
-		if (!isPasswordValid)
-			return res
-				.status(401)
-				.json({ success: false, message: "Invalid email or password" });
-		const userData = {
-			name: user.rows[0].name,
-			role: user.rows[0].role,
-			email: user.rows[0].email,
-			createdAt: user.rows[0].createdAt,
-			id: user.rows[0].id,
-		};
-		const token = signToken(JSON.stringify(userData));
-		return res.status(200).json({
-			success: true,
-			message: "successfully logged in",
-			token,
-		});
-	}
+  if (email == "admin@gmail.com" && password == "password") {
+    const userData = {
+      name: "Admin User",
+      role: "admin",
+      email: email,
+      createdAt: new Date(),
+      id: 1,
+    };
+    // const token = signToken(JSON.stringify(userData));
+    // const TokenInDatabase =await Token.create({token_content:token, userId:userData.id});
+    // console.log(TokenInDatabase);
+    return res.status(200).json({
+      success: true,
+      message: "successfully Admin logged in",
+      token,
+    });
+  } else {
+    if (!user.rowCount) {
+      return res
+        .status(401)
+        .send({ success: false, message: `Invalid email or password` });
+    }
+    const isPasswordValid = await verify(password, user.rows[0].password);
+    if (!isPasswordValid)
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid email or password" });
+    const userData = {
+      name: user.rows[0].name,
+      role: user.rows[0].role,
+      email: user.rows[0].email,
+      createdAt: user.rows[0].createdAt,
+      id: user.rows[0].id,
+    };
+    const token = signToken(JSON.stringify(userData));
+    return res.status(200).json({
+      success: true,
+      message: "successfully logged in",
+      token,
+    });
+  }
+};
+
+export const logOut = async (req, res) => {
+  const { cookies } = req;
+
+  const accessToken = req.headers.authorization;
+
+  if (!accessToken) {
+    return handleResponse(res, 400, { message: res.__("Login first") });
+  }
+  const cleanAccessToken = accessToken.replace("Bearer ", "");
+
+  const private_key = process.env.PRIVATE_KEY;
+  //
+try {
+	const logedInUser = jwt.verify(cleanAccessToken, private_key);
+	return handleResponse(res, 200, { message: res.__("Logout successfully") });
+	
+  } catch (error) {
+	return handleResponse(res, 200, { message: res.__("Invalid Token") });
+  }
+  
+
+    
+
+  // return handleResponse(res, 200, { message: res.__('logout successful') });
 };
--- src/Routes/signin.route.js
+++ src/Routes/signin.route.js
@@ -1,10 +1,10 @@
 /** @format */
 
 import express from "express";
-import { login } from "../controllers/Authentication/login.controller";
+import { login, logOut } from "../Controllers/Authentication/login.controller";
 
 const router = express.Router();
 
 router.post("/login", login);
-
+router.post("/logout",logOut)
 export default router;
