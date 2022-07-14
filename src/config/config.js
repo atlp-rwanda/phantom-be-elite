7f1569c570991b5b3c202afe9ed0ec18d2469897
SHYAKA Benjamin <59408657+benshyaka@users.noreply.github.com>
2022-07-14 13:46:49 +0200

    Fix(Resolving test issues) - Resolving tests - Resolving package-lock.json

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,7 @@ module.exports = {
     database: process.env.DB_DEVELOPMENT_NAME,
     host: process.env.HOST_FOR_DEVELOPMENT,
     port: process.env.PORT_FOR_DEVELOPMENT,
+    url: process.env.DEVELOPMENT_URI_DATABASE,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true,
@@ -20,6 +21,7 @@ module.exports = {
     database: process.env.DB_TEST_NAME,
     host: process.env.HOST_FOR_TEST,
     port: process.env.PORT_FOR_TEST,
+    url: process.env.TEST_URI_DATABASE,
     dialect: 'postgres',
     logging: false,
     protocol: "postgres",
@@ -36,6 +38,7 @@ module.exports = {
     database: process.env.DB_PRODUCTION_NAME,
     host: process.env.HOST_FOR_PRODUCTION,
     port: process.env.PORT_FOR_PRODUCTION,
+    url: process.env.PRODUCTION_URI_DATABASE,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
