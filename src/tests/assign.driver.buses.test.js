4eaa776da87e78cf92794d4f3d8ef802bc8cef13
shyaka axel divin <Xldivin@gmail.com>
2022-07-15 07:21:19 +0200

    ft(simulate)simulate
    some changes
    
    wip-14

--- src/tests/assign.driver.buses.test.js
+++ src/tests/assign.driver.buses.test.js
@@ -26,35 +26,7 @@ describe("POST API /api/v1/assign/", () => {
         return done();
       });
   });
-   it("Should be able to create the assign and delete the assign", function (done) {
-      const assign = {
-        route: "kayonza - nyandungu",
-        driver_name: "Aimable Nyagahungu",
-        plate_number: " RAD 239342 B",
-      };
-     chai
-       .request(index)
-       .post("/api/v1/assign/")
-       .send(assign)
-       .end((err, res) => {
-         if (err) return done(err);
-         expect(res).to.have.status(201);
-         expect(res.body).to.have.property("route");
-         expect(res.body).to.have.property("status");
-             // propagate the test block to delete the created assign using its id.
-             chai
-               .request(index)
-               .delete(`/api/v1/assign/${res.body.route[0].id}`)
-               .send()
-               .end((err, res) => {
-                 if (err) return done(err);
-                 expect(res).to.have.status([202]);
-                 expect(res.body).to.have.property("message");
-                 return done();
-               });
-           });
-       });
-   });
+  });
 
 describe("GET API /api/v1/assign", () => {
   it("Should return error message assign", (done) => {
