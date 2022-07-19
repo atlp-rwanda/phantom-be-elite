0502e37f27ab98deaad70237c7a0836c27da49dc
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-07-20 00:55:03 +0200

    feat(ft-simulate-bus-integration-28):  Simulating bus and mamaging large amount of data updating from the front-end using socket.io.
        - Installing socket.io and configure it.
        - Listening to the incoming data and sending responses directly through socket.
        - Updating the bus location in realtime by sending the updated location coordinates to the front-end
     at the specified time interval.
        [WIP #28]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import app from './app';
 import "dotenv/config";
+const http = require("http");
+const socketIO = require("socket.io");
 require("dotenv").config();
 
-
+// const PORT = 8080;
 const port = process.env.PORT || 3001;
-const server = app.listen(port, () => { console.log("Server listening on port " + port) });
+
+// spawnData();
+
+// const app = express();
+
+// spawnData();
+
+const server = http.createServer(app);
+const IOServer = socketIO(server, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+IOServer.on("connection", (socket) => {
+  socket.on("initialDataUpdate", (data) =>   socket.emit("initialData", data) );
+//   setupIntervalDataUpdates(socket);
+});
+
+server.listen(port, () => console.log("Server listening on port " + port));
+
+// const server = app.listen(port, () => { console.log("Server listening on port " + port) });
 
 export default server
